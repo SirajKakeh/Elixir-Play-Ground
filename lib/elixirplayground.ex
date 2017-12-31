4a0e09637ed5d16fc25822d6b7214ee31f6f8d11
SirajKakeh <sirajonline.89@gmail.com>
2017-12-31 18:41:16 +0200

    playing with file search

--- lib/elixirplayground.ex
+++ lib/elixirplayground.ex
@@ -12,7 +12,40 @@ defmodule Elixirplayground do
       :world
 
   """
-  def hello do
-    :world
+  import IO #, only: [puts: 1]
+  import File
+  import Path
+  def find(path \\ ".") do
+    if(Enum.at(wildcard("#{path}/*.exs"),0) != []) do
+      puts()
+    end
+    # if(regular?(path)) do
+    #   Path.wildcard("./*.exs")
+    #   |> Enum.at(0)
+    #   |> puts()
+    #   # puts Enum.at Path.wildcard("./*.exs"), 0
+    # end
+
+    # puts Path.wildcard("#{cwd!()}/**/*.wtev")
+
+    # cwd!()
+    # |> ls!()
+    # |> Enum.each(&puts &1)
   end
+
+  def ls_r(path \\ ".") do
+    cond do
+      File.regular?(path) -> [path]
+      File.dir?(path) ->
+        File.ls!(path)
+        |> Enum.map(&Path.join(path, &1)) #(fn (File.ls!(path)) -> Path.join(path, File.ls!(path)) end)
+        |> Enum.map(&ls_r/1)
+        |> Enum.join(" ..., ")
+      true -> []
+    end
+  end
+  
 end
+
+Elixirplayground.find()
+# IO.puts Filesearch.ls_r(File.cwd!())
