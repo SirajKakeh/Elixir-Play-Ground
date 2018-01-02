10a2a884628201b5469a504e2ef7c07bae7f6798
SirajKakeh <sirajonline.89@gmail.com>
2018-01-02 19:37:59 +0200

    working on file search, not done

--- lib/elixirplayground.ex
+++ lib/elixirplayground.ex
@@ -12,40 +12,74 @@ defmodule Elixirplayground do
       :world
 
   """
-  import IO #, only: [puts: 1]
-  import File
-  import Path
-  def find(path \\ ".") do
-    if(Enum.at(wildcard("#{path}/*.exs"),0) != []) do
-      puts()
-    end
-    # if(regular?(path)) do
-    #   Path.wildcard("./*.exs")
-    #   |> Enum.at(0)
-    #   |> puts()
-    #   # puts Enum.at Path.wildcard("./*.exs"), 0
-    # end
+  # import IO #, only: [puts: 1]
+  # import File
+  # import Path
 
-    # puts Path.wildcard("#{cwd!()}/**/*.wtev")
+  # def find(path \\ ".") do
+  #   Enum.map(ls!(path), fn (el) -> 
+  #     puts el
+  #     if(el |> dir?) do
+  #       find(el)
+  #     else
+  #       # puts(el)
+  #       if(String.match?(el, ~r/.srj/)) do
+  #         puts("fooooooooooooooooooooooound it at #{el}")
+  #       end
+  #     end
+  #   end)
+  # end
 
-    # cwd!()
-    # |> ls!()
-    # |> Enum.each(&puts &1)
-  end
-
-  def ls_r(path \\ ".") do
-    cond do
-      File.regular?(path) -> [path]
-      File.dir?(path) ->
-        File.ls!(path)
-        |> Enum.map(&Path.join(path, &1)) #(fn (File.ls!(path)) -> Path.join(path, File.ls!(path)) end)
-        |> Enum.map(&ls_r/1)
-        |> Enum.join(" ..., ")
-      true -> []
+  def find(filepath) do
+    if(String.match?(filepath, ~r/.srj/)) do
+      IO.puts("ooooooooooooooooooooooooooooooooooooooooooooooooooooook #{filepath}")
+    else
+      expand(File.ls(filepath), filepath)
     end
   end
-  
+
+  def expand({:ok, files}, path) do
+    files
+    |> Enum.flat_map(fn (el) -> find("#{path}/#{el}")
+    end)
+  end
+
+  def expand({:error, _}, path) do 
+    [path]
+  end
+
+
+  # def find(path \\ ".") do
+  #   cond do
+  #     Path.wildcard(Path.join(path, "*.{srj}")) != [] ->
+  #       IO.puts("successsssssssssssssssssssssssssssssssss at #{path}")
+  #     File.dir?(path) ->
+  #       File.ls!(path)
+  #       |> Enum.map(&Path.join(path, &1)) #(File.ls!(path), Path.join(path, File.ls!(path)) end)
+  #       |> Enum.map(&find/1)
+  #       |> Enum.concat()
+  #       |> IO.puts()
+  #     true -> []
+  #   end
+  # end
+
+  # def list(path \\ ".") do
+  #   cond do
+  #     File.regular?(path) -> [path]
+  #     File.dir?(path) ->
+  #       File.ls!(path)
+  #       |> Enum.map(&Path.join(path, &1)) #(fn (File.ls!(path)) -> Path.join(path, File.ls!(path)) end)
+  #       |> Enum.map(&list/1)
+  #       |> Enum.concat()
+  #     true -> []
+  #   end
+  # end
+
+  # def testRegex(str, regex) do
+  #   IO.puts(String.match?(str, regex))
+  # end
 end
 
-Elixirplayground.find()
+Elixirplayground.find(".")
 # IO.puts Filesearch.ls_r(File.cwd!())
+# Elixirplayground.testRegex(".aasiakllprime/ujgnwf.srj", ~r/.srj/)
